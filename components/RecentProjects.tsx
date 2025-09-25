c475c4a75d8f4c41eec9e7e2e06b1230fc4bbbe1
Malinde Godwin <malindegodwin9@gmail.com>
2025-09-25 19:02:05 +0300

    initial commit

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { projects } from "@/data";
 import { PinContainer } from "@/components/ui/3d-pin";
+import { FaLocationArrow } from "react-icons/fa";
 
 const RecentProjects = () => {
   return (
@@ -9,15 +10,15 @@ const RecentProjects = () => {
         A small selection of {""}
         <span className="text-purple">Recent projects</span>
       </h1>
-      <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
+      <div className="flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10">
         {projects.map(({ id, title, des, img, iconLists, link }) => (
           <div
             key={id}
-            className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
+            className="sm:h-[41rem] h-[32rem] xl:min-h-[32.5rem] flex items-center justify-center sm:w-96 w-[80vw]"
           >
-            <PinContainer title={title} href={link}>
-              <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden  h-[35vh] lg:h-[40vh] mb-10">
-                <div>
+            <PinContainer title={link} href={link}>
+              <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden  h-[30vh]  mb-10">
+                <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
                   <img src="/bg.png" alt="bg-img" />
                 </div>
                 <img src={img} alt={title} className="absolute z-10 bottom-0" />
@@ -40,6 +41,12 @@ const RecentProjects = () => {
                       <img src={icon} alt={icon} className="p-2" />
                     </div>
                   ))}
+                </div>
+                <div className="flex justify-center items-center">
+                  <p className="flex lg:text-xl md:text-xs text-sm text-purple">
+                    Check Live Site
+                  </p>
+                  <FaLocationArrow className="ms-3" color="#CBACF(" />
                 </div>
               </div>
             </PinContainer>

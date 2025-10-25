b274449efd77a720c66ce1eef52e9b1ab8ad26c0
Malinde Godwin <malindegodwin9@gmail.com>
2025-10-25 12:17:07 +0300

    first commit

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { InfiniteMovingCards } from "./ui/InfiniteMovingCards";
+import { testimonials } from "@/data";
 
 const Clients = () => {
   return (
@@ -7,7 +9,15 @@ const Clients = () => {
         Kind words from{""}
         <span className="text-purple"> satisfied clients</span>
       </h1>
-      <div className="flex flex-col items-center justify-center p-4 gap-x-24 gap-y-8 mt-10"></div>
+      <div className="flex flex-col items-center">
+        <div className="flex flex-col antialiased items-center relative overflow-hidden">
+          <InfiniteMovingCards
+            items={testimonials}
+            direction="right"
+            speed="slow"
+          />
+        </div>
+      </div>
     </div>
   );
 };

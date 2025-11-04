e93b2d25707026e2b33cdd5b680e746680dbcf11
Malinde Godwin <malindegodwin9@gmail.com>
2025-11-04 16:09:26 +0300

    intial commit

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Experience = () => {
-  return <div></div>;
+  return (
+    <div className="py-20" id="testimonals">
+      <h1 className="heading">
+        My
+        <span className="text-purple"> work experience</span>
+      </h1>
+      <div className="w-full mt-12 grid lg:grid-cols-4 grid-cols-1"></div>
+    </div>
+  );
 };
 
 export default Experience;

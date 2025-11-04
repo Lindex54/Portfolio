ebe71316281b1e30305838f1169f13af567ad44d
Malinde Godwin <malindegodwin9@gmail.com>
2025-11-04 17:20:08 +0300

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import Grid from "@/components/Grid";
 import RecentProjects from "@/components/RecentProjects";
 import { navItems } from "@/data";
 import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
+import Approach from "@/components/Approach";
 
 export default function Home() {
   return (
@@ -19,7 +21,9 @@ export default function Home() {
         <Hero />
         <Grid />
         <RecentProjects />
-        <Clients />
+        {/* <Clients /> */}
+        <Experience />
+        <Approach />
       </div>
     </main>
   );
